0e4825d6ad2e02f1777a956342879d50c7876ecb
Aung Myo Aye <aungmyoaye101@gmail.com>
2024-12-21 13:21:07 +0630

    feat: enhance NavBar with Menubar for theme switching and improve layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,29 +4,44 @@ import React from "react";
 import Link from "next/link";
 import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import { useTheme } from "./Theme";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 const NavBar: React.FC = () => {
   const { mode, setMode } = useTheme();
   return (
     <nav className="flex justify-between items-center p-4 md:px-10 bg-gray-800 text-white fixed top-0 z-50 w-full">
-      <div className="">
-        <Link href="/">Dev-Overflow</Link>
+      <div className="text-2xl font-poppins font-bold">
+        <Link href="/">
+          <span>Dev</span>
+          <span className="text-orange">Overflow</span>
+        </Link>
       </div>
-      <div className="flex items-start gap-4">
-        <button onClick={() => setMode("dark")} className="text-orange">
-          dark
-        </button>
-        <button onClick={() => setMode("light")} className="text-light-gray">
-          light
-        </button>
+      <div className="flex items-center gap-4">
+        <Menubar className="relative">
+          <MenubarMenu>
+            <MenubarTrigger>
+              {mode === "dark" ? "dark" : "light"}
+            </MenubarTrigger>
+            <MenubarContent className=" absolute top-4 -right-8 w-fit">
+              <MenubarItem onClick={() => setMode("light")}>Light</MenubarItem>
+
+              <MenubarSeparator />
+              <MenubarItem onClick={() => setMode("dark")}>Dark</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>System</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
 
         <UserButton />
-        <Link href="/signin">
-          <SignInButton />
-        </Link>
-        <Link href="/signout">
-          <SignOutButton />
-        </Link>
       </div>
     </nav>
   );
