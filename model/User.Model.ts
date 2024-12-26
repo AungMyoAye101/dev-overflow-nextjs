8a4b40b81e040a899bc6d84085e35a2394bfd61b
Aung Myo Aye <aungmyoaye101@gmail.com>
2024-12-26 18:29:41 +0630

    feat: enhance User model with additional fields and validation requirements

--- model/User.Model.ts
+++ model/User.Model.ts
@@ -1,5 +1,68 @@
 import { Schema, models, model, Document } from "mongoose";
 
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture?: string;
+  location?: string;
+  protfolio?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
 const UserSchema = new Schema({
-  clerkId: String,
+  clerkId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  picture: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  protfolio: {
+    type: String,
+  },
+  reputation: {
+    type: Number,
+    default: 0,
+  },
+  saved: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Question",
+    },
+  ],
+  joinedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+const User = models.User || model<IUser>("User", UserSchema);
+export default User;
