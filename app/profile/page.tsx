1bf1619605ca18c3cf7c4518002f7173dc6c9b7b
Aung Myo Aye <aungmyoaye101@gmail.com>
2024-12-27 19:28:46 +0630

    feat: add Stats component to Profile page and enhance layout; include new badge icons

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,12 @@
 import Profile from "@/components/Profile";
+import Stats from "@/components/Stats";
 import React from "react";
 
 const page = () => {
   return (
-    <section className="pt-[8rem] pb-10 px-4 md:px-10">
+    <section className="pt-[8rem] pb-10 px-4 md:px-10 space-y-6">
       <Profile />
+      <Stats />
     </section>
   );
 };
---
+++ components/Stats.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import React from "react";
+
+const Stats = () => {
+  return (
+    <section className="space-y-4">
+      <h1 className="h2-bold">Stats</h1>
+      <div className="flex flex-wrap gap-4">
+        <div className="bg_dark_white shadow_rounded py-4 px-6 flex flex-col items-center justify-center  font-noto_serif font-semibold ">
+          <div className="space-x-2 ">
+            <span>100</span>
+            <span>Question</span>
+          </div>
+          <div className="space-x-2  ">
+            <span>100</span>
+            <span>Answers</span>
+          </div>
+        </div>
+
+        <div className="bg_dark_white shadow_rounded py-4 px-6 flex items-center gap-4">
+          <Image
+            src={"/assets/icons/gold.svg"}
+            width={40}
+            height={40}
+            alt="gold badge"
+          />
+          <div className="flex flex-col font-noto_serif font-semibold text-sm">
+            <span>100</span>
+            <span>Gold Bage</span>
+          </div>
+        </div>
+        <div className="bg_dark_white shadow_rounded py-4 px-6 flex items-center gap-4">
+          <Image
+            src={"/assets/icons/silver.svg"}
+            width={40}
+            height={40}
+            alt="silver badge"
+          />
+          <div className="flex flex-col font-noto_serif font-semibold text-sm">
+            <span>100</span>
+            <span>Silver Bage</span>
+          </div>
+        </div>
+        <div className="bg_dark_white shadow_rounded py-4 px-6 flex items-center gap-4">
+          <Image
+            src={"/assets/icons/bronze.svg"}
+            width={40}
+            height={40}
+            alt="bronze badge"
+          />
+          <div className="flex flex-col font-noto_serif font-semibold text-sm">
+            <span>100</span>
+            <span>Bronze Bage</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
