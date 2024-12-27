0acb193baf644c8d271f94c96723abd53eabccdd
Aung Myo Aye <aungmyoaye101@gmail.com>
2024-12-27 18:28:10 +0630

    feat: enhance RightSideBar component by updating tag section with badges and improved labeling

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaGreaterThan } from "react-icons/fa";
+import { Badge } from "./ui/badge";
 
 const RightSideBar = () => {
   return (
@@ -52,41 +53,24 @@ const RightSideBar = () => {
         {/* tags container */}
         <div className="flex flex-col gap-4">
           <div>
-            <h1 className="text-xl font-poppins font-semibold">Tags</h1>
+            <h1 className="text-xl font-poppins font-semibold">Popular Tags</h1>
           </div>
-          <div className="flex flex-wrap gap-4">
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
+          <div className="flex flex-col gap-4 font-semibold font-poppins">
+            <button className="flex justify-between items-center">
+              <Badge className="px-4 py-2 font-noto_serif ">Next js</Badge>
+              <span>1069</span>
             </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
+            <button className="flex justify-between items-center">
+              <Badge className="px-4 py-2 font-noto_serif ">Next js</Badge>
+              <span>1069</span>
             </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
+            <button className="flex justify-between items-center">
+              <Badge className="px-4 py-2 font-noto_serif ">Next js</Badge>
+              <span>1069</span>
             </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Node
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Node
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
-            </button>
-            <button className="rounded-full px-3 py-1 bg-light-gray dark:bg-dark-gray shadow dark:shadow-none">
-              Next js{" "}
+            <button className="flex justify-between items-center">
+              <Badge className="px-4 py-2 font-noto_serif ">Next js</Badge>
+              <span>1069</span>
             </button>
           </div>
         </div>
