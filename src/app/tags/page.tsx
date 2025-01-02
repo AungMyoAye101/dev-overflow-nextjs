bcf9be47e0c88e6cdbc2d565cfd861ecb431ce82
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-01-02 12:34:40 +0630

    feat: implement tag fetching and display in Tags page

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,26 +1,31 @@
 import LocalSearchBox from "@/src/components/LocalSearchBox";
 import { Badge } from "@/src/components/ui/badge";
+import { getAllTags } from "@/src/lib/actions/get.tags";
+
 import React from "react";
 
-const page = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const Page = async () => {
+  const tags = await getAllTags();
+  console.log("page", tags);
+  if (!tags) return <div>Loading...</div>;
+
   return (
     <section className="page_padding">
       <h1 className="h1-bold">All Tags</h1>
       <LocalSearchBox />
       <div className="grid sm:grid-cols-2  lg:grid-cols-3 gap-4 place-items-center">
-        {arr.map((i) => (
+        {tags.map((tag) => (
           <div
-            key={i}
+            key={tag._id}
             className="bg_dark_white flex flex-col  gap-4 px-4 py-6 shadow_rounded w-fit"
           >
-            <Badge className="py-2 px-4 w-fit">Next JS</Badge>
+            <Badge className="py-2 px-4 w-fit">{tag.name}</Badge>
             <p className="text-sm font-noto_serif line-clamp-3">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
-              optio sed modi reprehenderit ipsa excepturi
+              optio sed modi
             </p>
             <div className="font-noto_serif flex gap-2 text-sm">
-              <span className="text-orange">1+</span>
+              <span className="text-orange">{tag.questions.length} +</span>
               <span>Question</span>
             </div>
           </div>
@@ -30,4 +35,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- src/lib/actions/get.tags.ts
+++ src/lib/actions/get.tags.ts
@@ -1,12 +1,28 @@
 "use server";
 
+import { Question } from "@/src/components/Question";
 import connectToDB from "@/src/database/db";
 import Tags from "@/src/model/Tag.model";
+import { NextResponse } from "next/server";
 
 export const getTagById = async (id: string) => {
   try {
     await connectToDB();
     const tags = await Tags.findById(id);
+    return tags;
+  } catch (error: any) {
+    console.log("Failed to fetch tags", error.message);
+    throw error;
+  }
+};
+export const getAllTags = async () => {
+  try {
+    await connectToDB();
+    const tags = await Tags.find().populate({
+      path: "questions",
+      select: "_id title description",
+    });
+    console.log(tags);
     return tags;
   } catch (error: any) {
     console.log("Failed to fetch tags", error.message);
