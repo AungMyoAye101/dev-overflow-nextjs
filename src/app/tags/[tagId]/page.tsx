13f32251f2c94c6a86d2c3f7244a7e500c42cc9b
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-01-07 22:59:50 +0630

    feat:add get question by tag id

--- src/app/tags/[tagId]/page.tsx
+++ src/app/tags/[tagId]/page.tsx
@@ -1,8 +1,12 @@
+import { getQuestionByTagId } from "@/src/lib/actions/tags.action";
 import React from "react";
 
 const page = async ({ params }: { params: Promise<{ tagId: string }> }) => {
   const { tagId } = await params;
-  console.log(tagId);
+  const result = await getQuestionByTagId({
+    tagId,
+  });
+  console.log("result", result);
   return (
     <section className="page_padding">
       <div>{tagId}</div>
--- src/lib/actions/tags.action.ts
+++ src/lib/actions/tags.action.ts
@@ -1,6 +1,8 @@
 "use server";
 import connectToDB from "@/src/database/db";
+import Question from "@/src/model/question.model";
 import Tags from "@/src/model/Tag.model";
+import User from "@/src/model/User.Model";
 
 export const getTagById = async (id: string) => {
   try {
@@ -23,6 +25,37 @@ export const getAllTags = async () => {
     return tags;
   } catch (error: any) {
     console.log("Failed to fetch tags", error.message);
+    throw error;
+  }
+};
+
+export const getQuestionByTagId = async (params: any) => {
+  const { tagId } = params;
+  try {
+    const tag = await Tags.findById(tagId).populate({
+      path: "questions",
+      model: Question,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        {
+          path: "author",
+          model: User,
+        },
+        {
+          path: "tags",
+          model: Tags,
+        },
+      ],
+    });
+    if (!tag) {
+      throw new Error("Tags not found");
+    }
+    console.log("successfully fetch questions by tag");
+    return { name: tag.name, questions: tag.questons };
+  } catch (error: any) {
+    console.log(error.message);
     throw error;
   }
 };
