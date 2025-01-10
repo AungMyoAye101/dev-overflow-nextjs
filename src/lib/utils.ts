e14d6b316c22333cc4024efee435fdca26398aab
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-01-10 20:17:47 +0630

    feat: add query-string dependency and implement formQuery and removeFormQuery utility functions for URL manipulation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { formatDistanceToNow } from "date-fns";
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +10,43 @@ export function cn(...inputs: ClassValue[]) {
 export const timestamp = (time: Date) => {
   const formatDate = formatDistanceToNow(new Date(time), { addSuffix: true });
   return formatDate;
+};
+
+interface FormQueryParams {
+  params: string;
+  key: string;
+  value: string;
+}
+
+export const formQuery = ({ params, key, value }: FormQueryParams) => {
+  const currentUrl = qs.parse(params);
+  currentUrl[key] = value;
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+};
+
+interface RemoveFormQueryParams {
+  params: string;
+  keyToRemove: string[];
+}
+export const removeFormQuery = ({
+  params,
+  keyToRemove,
+}: RemoveFormQueryParams) => {
+  const currentUrl = qs.parse(params);
+  keyToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
 };
