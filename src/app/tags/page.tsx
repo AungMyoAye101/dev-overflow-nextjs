030cdfdb0cc083730d87d2a7f38d7942c6c0c920
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-01-12 18:09:47 +0630

    feat: implement pagination for tags in the tags page and update getAllTags function

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,5 +1,6 @@
 import Filter from "@/src/components/Filter";
 import LocalSearchBox from "@/src/components/LocalSearchBox";
+import PaginationBox from "@/src/components/PaginationBox";
 import { Badge } from "@/src/components/ui/badge";
 import { filteredSearch, sortTags } from "@/src/constants";
 import { getAllTags } from "@/src/lib/actions/tags.action";
@@ -10,11 +11,12 @@ import React from "react";
 
 const Page = async ({ searchParams }: SearchParamsProps) => {
   const query = await searchParams;
-  const tags = await getAllTags({
+  const results = await getAllTags({
     searchQuery: query.q,
     sortQuery: query.filter,
+    page: query.page ? +query.page : 1,
   });
-  if (!tags) return <div>Loading...</div>;
+  if (!results) return <div>Loading...</div>;
 
   return (
     <section className="page_padding">
@@ -24,7 +26,7 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
         <Filter filterArray={sortTags} />
       </div>
       <div className="grid sm:grid-cols-2  lg:grid-cols-3 gap-4 place-items-center">
-        {tags.map((tag) => (
+        {results.tags.map((tag) => (
           <div
             key={tag._id}
             className="bg_dark_white flex flex-col  gap-4 px-4 py-6 shadow_rounded w-fit"
@@ -43,6 +45,10 @@ const Page = async ({ searchParams }: SearchParamsProps) => {
           </div>
         ))}
       </div>
+      <PaginationBox
+        pageNumber={query?.page ? +query.page : 1}
+        isNext={results.isNext}
+      />
     </section>
   );
 };
