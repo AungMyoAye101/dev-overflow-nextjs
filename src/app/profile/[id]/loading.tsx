b4947ac62f43d05d4bd0e105c4325ebeab6c90d1
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-01-18 13:01:51 +0630

    feat: improve loading skeleton design in profile component for better visual hierarchy

--- src/app/profile/[id]/loading.tsx
+++ src/app/profile/[id]/loading.tsx
@@ -4,19 +4,24 @@ import React from "react";
 const ProfileLoading = () => {
   return (
     <section className="page_padding">
-      <div className="bg_dark_white p-4 rounded-md shadow dark:shadow-none flex ">
-        <Skeleton className="w-14 h-14 rounded-full" />
-        <div className="flex flex-col gap-1">
-          <Skeleton className="w-24 h-8" />
-          <Skeleton className="w-24 h-8" />
-          <div className="flex gap-4">
-            <Skeleton className="w-20 h-10" />
-            <Skeleton className="w-20 h-10" />
-            <Skeleton className="w-20 h-10" />
+      <div className="bg_dark_white p-4 rounded-md shadow dark:shadow-none flex justify-between ">
+        <div className="flex gap-4">
+          <Skeleton className="w-20 h-20 rounded-full" />
+          <div className="flex flex-col gap-1">
+            <Skeleton className="w-40 h-4" />
+            <Skeleton className="w-40 h-4" />
+            <Skeleton className="w-40 h-4 mt-2" />
+            <div className="flex gap-4 mt-2">
+              <Skeleton className="w-20 h-6" />
+              <Skeleton className="w-20 h-6" />
+              <Skeleton className="w-20 h-6" />
+            </div>
           </div>
         </div>
+
         <Skeleton className="w-20 h-10" />
       </div>
+      <Skeleton className="w-20 h-10" />
       <div className="flex flex-wrap gap-4">
         <Skeleton className="w-40 h-20" />
         <Skeleton className="w-40 h-20" />
